546f893609aac92ea622d14b4d64a90b48dd3339
Luc Poupard <poupard.luc@kloh.fr>
2020-12-21 18:32:22 +0100

    Update home content

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
+const moment = require("moment");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/fonts");
+  // date filter (localized)
+  // https://jamstatic.fr/2019/09/07/site-multilingue-avec-eleventy/
+  // https://momentjs.com/
+  eleventyConfig.addNunjucksFilter("yearsToNow", function (date) {
+    return moment().diff(date, "years");
+  });
+
 
   return {
     dir: {
